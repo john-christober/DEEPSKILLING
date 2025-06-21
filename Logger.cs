c6b6929107e94a3676e585ccb87c887702665ad2
john-christober <thomasepzi79@gmail.com>
2025-06-21 19:00:06 +0530

    Initial commit for singleton pattern project

---
+++ Logger.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Logger
+{
+    private static Logger ? instance;
+    private static readonly object lockObj = new object();
+    private Logger()
+    {
+        Console.WriteLine("Logger Initialized");
+    }
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            lock (lockObj)
+            {
+                if (instance == null)
+                {
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    // Method to simulate logging
+    public void Log(string message)
+    {
+        Console.WriteLine("Log: " + message);
+    }
+}
