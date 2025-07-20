dd068ab2e3222a28f88aec8c27640a4557cafcdc
john-christober <145186233+john-christober@users.noreply.github.com>
2025-07-20 17:02:37 +0530

    Add files via upload

---
+++ JwtAuthDemo/Controllers/SecureController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class SecureController : ControllerBase
+{
+    [HttpGet("data")]
+    public IActionResult GetData()
+    {
+        return Ok("You have access to a protected endpoint!");
+    }
+}
